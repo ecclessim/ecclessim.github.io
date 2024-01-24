d01168647d2d0cf1c4660128fc354078469d6cec
Eccles Sim <eccles97@outlook.com>
2024-01-24 22:54:19 +0800

    added download resume

--- ecc-pro-page/src/App.js
+++ ecc-pro-page/src/App.js
@@ -2,10 +2,9 @@ import React from "react";
 import "./App.css";
 import ExperienceItem from "./components/ExperienceItem";
 import experienceData from "./static/resumeJson.json";
+import resumePdf from "./static/ecclesResume.pdf";
 
 export default function App() {
-  const currentRole = "development analyst";
-  const currentCompany = "standard chartered";
   const schoolExp = {
     dateStart: "aug 2019",
     dateEnd: "dec 2022",
@@ -29,6 +28,20 @@ export default function App() {
       </h3>
       <div className="body-text">
         <p>walking my path in the finance/technology space as a developer.</p>
+        <p>
+          download my{" "}
+          <strong>
+            <a
+              className="text-info"
+              href={resumePdf}
+              target="_blank"
+              rel="noreferrer"
+            >
+              {" "}
+              &lt;resume&gt;
+            </a>
+          </strong>
+        </p>
       </div>
       <div className="section">
         <h5>experience</h5>
